01d758f1044df48b6d3ab19b9271a4e3d1344cdb
Rajesh Palepu <rajesh.palepu@homeoffice.wal-mart.com>
2019-12-15 18:53:41 -0600

    make it better

--- index.js
+++ index.js
@@ -81,6 +81,7 @@ const resolvers = {
 const server = new ApolloServer({ typeDefs, resolvers });
 
 // The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+// The `listen` method launches a web server.
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
 });
